71885235b04990ec4fffb5036573a939a09ccaa0
SylvainBnstn <36404524+SylvainBnstn@users.noreply.github.com>
2018-04-05 22:02:05 +0200

    boutons

--- graph_lib_exemple_graph/graph.cpp
+++ graph_lib_exemple_graph/graph.cpp
@@ -151,12 +151,92 @@ GraphInterface::GraphInterface(int x, int y, int w, int h)
     m_tool_box.set_gravity_xy(grman::GravityX::Left, grman::GravityY::Up);
     m_tool_box.set_bg_color(BLANCBLEU);
 
+    ///check box pour le graph reduit
+
+    m_tool_box.add_child(m_check_box_1);
+    m_check_box_1.set_value(true);
+    m_check_box_1.set_dim(20,20);
+    m_check_box_1.set_gravity_xy(grman::GravityX::Left, grman::GravityY::Up);
+    m_check_box_1.set_bg_color(BLANCROSE);
+
+    ///boite englobant les bouttons d'ajout et de suppression
+
+    m_tool_box.add_child(m_button_global);
+    m_button_global.set_dim(76,76);
+    m_button_global.set_gravity_y(grman::GravityY::Down);
+    m_button_global.set_bg_color(BLANCROSE);
+
+    ///bouton ajout edge en haut a gauche de la boite
+
+    m_button_global.add_child(m_button_add_edge);
+    m_button_add_edge.set_dim(31,31);
+    m_button_add_edge.set_gravity_xy(grman::GravityX::Left, grman::GravityY::Up);
+    m_button_add_edge.set_bg_color(VERTCLAIR);
+
+    m_button_add_edge.add_child(m_txt_a_e);
+    m_txt_a_e.set_message("A-E");
+
+    ///boutton suppression edge en haut a droite de la boite
+
+    m_button_global.add_child(m_button_del_edge);
+    m_button_del_edge.set_dim(31,31);
+    m_button_del_edge.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Up);
+    m_button_del_edge.set_bg_color(ROUGECLAIR);
+
+    m_button_del_edge.add_child(m_txt_d_e);
+    m_txt_d_e.set_message("D-E");
+
+    ///bouton ajout vertex en bas a gauche de la boite
+
+    m_button_global.add_child(m_button_add_vertex);
+    m_button_add_vertex.set_dim(31,31);
+    m_button_add_vertex.set_gravity_xy(grman::GravityX::Left, grman::GravityY::Down);
+    m_button_add_vertex.set_bg_color(VERT);
+
+    m_button_add_vertex.add_child(m_txt_a_v);
+    m_txt_a_v.set_message("A-V");
+
+    ///bouton ajout vertex en bas a gauche de la boite
+
+    m_button_global.add_child(m_button_reset);
+    m_button_reset.set_dim(31,31);
+    m_button_reset.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Down);
+    m_button_reset.set_bg_color(ROUGE);
+
+    m_button_reset.add_child(m_txt_reset);
+    m_txt_reset.set_message("Res");
+
     m_top_box.add_child(m_main_box);
     m_main_box.set_dim(908,720);
     m_main_box.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Up);
     m_main_box.set_bg_color(BLANCJAUNE);
 }
 
+
+
+void GraphInterface::ajout_suppression()
+{
+    if(m_button_add_edge.get_value()==true)
+    {
+        std::cout<<"1"<<std::endl;
+        m_button_add_edge.set_value(false);
+    }
+    if(m_button_del_edge.get_value()==true)
+    {
+        std::cout<<"2"<<std::endl;
+        m_button_del_edge.set_value(false);
+    }
+    if(m_button_add_vertex.get_value() ==true)
+    {
+        std::cout<<"3"<<std::endl;
+        m_button_add_vertex.set_value(false);
+    }
+        if(m_button_reset.get_value() ==true)
+    {
+        std::cout<<"4"<<std::endl;
+        m_button_reset.set_value(false);
+    }
+}
 
 /// Méthode spéciale qui construit un graphe arbitraire (démo)
 /// Cette méthode est à enlever et remplacer par un système
@@ -242,7 +322,7 @@ void Graph::boucle(std::string name)
     {
         /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
         update();
-
+	m_interface->ajout_suppression();
         /// Mise à jour générale (clavier/souris/buffer etc...)
         grman::mettre_a_jour();
     }
--- graph_lib_exemple_graph/graph.h
+++ graph_lib_exemple_graph/graph.h
@@ -272,6 +272,23 @@ class GraphInterface
         /// Dans cette boite seront ajoutés des boutons de contrôle etc...
         grman::WidgetBox m_tool_box;
 
+        ///check box pour le graph reduit
+        grman::WidgetCheckBox m_check_box_1;
+
+        ///boite englobant les bouttons d'ajout et de suppression
+        grman::WidgetBox m_button_global;
+
+        ///bouttons ajout et supression edge et vertex
+        grman::WidgetButton m_button_add_edge;
+        grman::WidgetButton m_button_del_edge;
+        grman::WidgetButton m_button_add_vertex;
+        grman::WidgetButton m_button_reset;
+
+        grman::WidgetText m_txt_a_e;
+        grman::WidgetText m_txt_d_e;
+        grman::WidgetText m_txt_a_v;
+        grman::WidgetText m_txt_reset;
+
         // A compléter éventuellement par des widgets de décoration ou
         // d'édition (boutons ajouter/enlever ...)
 
@@ -279,6 +296,7 @@ class GraphInterface
 
         // Le constructeur met en place les éléments de l'interface
         // voir l'implémentation dans le .cpp
+        void ajout_suppression();
         GraphInterface(int x, int y, int w, int h);
 };
 
