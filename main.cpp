150108cc11f60c5e15118b5d839f14488ab78f8d
JProuvost <35262419+JProuvost@users.noreply.github.com>
2018-04-06 16:46:02 +0200

    Interface graphique fini
    il ne manque que les parties allant avec les études fonctionnelles et dynamique.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "grman/grman.h"
+#include <iostream>
+
+#include "graph.h"
+
+int main()
+{
+    int xm,ym;
+    /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("pics");
+
+    /// Un exemple de graphe
+    Graph g;
+
+    g.menu();
+
+    grman::fermer_allegro();
+    return 0;
+}
+END_OF_MAIN();
+
